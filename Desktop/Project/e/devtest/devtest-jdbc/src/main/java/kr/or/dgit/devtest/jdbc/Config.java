64b3ddaecd878482c20014dc7a0a9a50d0b1a416
ParangLuv <redesign628@naver.com>
2017-05-10 12:40:24 +0900

    sec

--- Desktop/Project/e/devtest/devtest-jdbc/src/main/java/kr/or/dgit/devtest/jdbc/Config.java
+++ Desktop/Project/e/devtest/devtest-jdbc/src/main/java/kr/or/dgit/devtest/jdbc/Config.java
@@ -9,4 +9,12 @@ public class Config {
     public static final String PWD    = "user_ncs";
     public static final String URL    = "jdbc:mysql://localhost:3306/"+DB_NAME;
     public static final String DRIVER = "com.mysql.jdbc.Driver";
+
+
+    public static final String[] TABLE_NAME = {"title","employee", "department"};
+
+    public static String Export_path = "";
+    public static String Import_path = "";
+    public static final String MYSQL_EXPORT_PATH = "C:\\ProgramData\\MySQL\\MySQL Server 5.6\\Uploads\\";
+
 }
